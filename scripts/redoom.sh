7cec4a6a2a4b8a279f50e82ff59819c63e18a27c
Ivan Erakhtin <ivan.erakhtin@gmail.com>
2024-05-15 15:47:04 +0300

    Update redoom.sh

--- scripts/redoom.sh
+++ scripts/redoom.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
 sudo pacman -R emacs
-rm -rf ~/.emacs.d
+rm -rf ~/.config/emacs
 
 sudo pacman -S emacs
 
 git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
 ~/.config/emacs/bin/doom install
 
-rm -rf ~/.doom.d
-git clone https://github.com/ier/.doom.d.git ~/.doom.d
+rm -rf ~/.config/doom
+git clone https://github.com/ier/.doom.d.git ~/.config/doom
 
-./doom sync && ./doom build
+~/.config/emacs/bin/doom sync && ~/.config/emacs/bin/doom build
 
-sudo pacman -S ripgrep fd cmake
-sudo pacman -S ttc-iosevka
+sudo pacman -S ripgrep fd cmake ttc-iosevka
 
 ;; deprecated: M-x nerd-icons-install-fonts RET
 ;; M-x all-the-icons-install-fonts RET
