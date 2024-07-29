6fd76669bb8fe1567039c71cefd89aa0e2562f90
Ivan Erakhtin <ivan.erakhtin@gmail.com>
2024-07-29 21:30:58 +0300

    Update redoom.sh
    Use ssh clone method

--- scripts/redoom.sh
+++ scripts/redoom.sh
@@ -9,7 +9,7 @@ git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
 ~/.config/emacs/bin/doom install
 
 rm -rf ~/.config/doom
-git clone https://github.com/ier/.doom.d.git ~/.config/doom
+git clone git@github.com:ier/.doom.d.git ~/.config/doom
 
 ~/.config/emacs/bin/doom sync && ~/.config/emacs/bin/doom build
 
